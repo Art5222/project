3f69e8e648a7e28f61c0e8e5bc079a3b32e1dd6f
Esim <example@gmail.com>
2021-09-13 03:22:37 +0400

    homework

--- js/style.js
+++ js/style.js
@@ -1,3 +1,14 @@
 'use strict';
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?',' ');
 
-let key = 55;
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+personalMovieDB.movies[prompt('Один из последных просмотренных фильмов ?',' ')]=+prompt('На сколько оцените его?',' ');
+personalMovieDB.movies[prompt('Один из последных просмотренных фильмов ?',' ')]=+prompt('На сколько оцените его?',' ');
+
+
